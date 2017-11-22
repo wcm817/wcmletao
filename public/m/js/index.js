ff8e160735f449ca6cfe71d5555a77286e41e001
wucaimei <wucaimei817@163.com>
2017-11-22 15:53:17 +0800

    第一次提交,index.html 和未完成的category.html

--- public/m/js/index.js
+++ public/m/js/index.js
@@ -1,8 +1,12 @@
-/*��ʼ������������*/
-mui('.mui-scroll-wrapper').scroll({
-    indicators:false
-});
-/*�ֲ�ͼ�ĳ�ʼ��*/
-mui('.mui-slider').slider({
-    interval:4000
-});
+$(function() {
+    /*初始化区域滚动*/
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+    //获得slider插件对象------ 轮播图
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval: 1500 //自动轮播周期，若为0则不自动播放，默认为0；
+    });
+})
